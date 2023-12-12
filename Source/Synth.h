d54be353d03aea6a82f1e8ff01536576b2698378
rjbaldwin <113042381+rjbaldwin@users.noreply.github.com>
2023-12-12 12:29:52 +0000

    First commit

---
+++ Source/Synth.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    Synth.h
+    Created: 12 Dec 2023 11:31:05am
+    Author:  rjbal
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "Voice.h"
+
+class Synth
+{
+public:
+    Synth();
+
+    void allocateResources(double sampleRate_, int samplesPerBlock);
+    void deallocateResources();
+    void reset();
+    void render(float** outputBuffers, int sampleCount);
+    void midiMessage(uint8_t data0, uint8_t data1, uint8_t data2);
+
+private:
+    float sampleRate;
+    Voice voice;
+
+    void noteOn(int note, int velocity);
+    void noteOff(int note);
+};
