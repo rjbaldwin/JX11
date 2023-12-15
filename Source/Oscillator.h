38045d7719822c29095d2dcf258703b7dcbc0584
rjbaldwin <113042381+rjbaldwin@users.noreply.github.com>
2023-12-15 10:29:11 +0000

    pulse train added

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -10,34 +10,63 @@
 
 #pragma once
 
-
+const float PI_OVER_4{ 0.7853987633974483f };
+const float PI{ 3.1415926535897932f };
 const float TWO_PI{ 6.2831853071795864f };
+
 
 class Oscillator
 {
 public:
-    float amplitude{};
-    float inc{};
-    float phase{};
-
+    float period{ 0.0f };
+    float amplitude{ 1.0f };
 
     void reset()
     {
+        inc = 0.0f;
         phase = 0.0f;
-        
     }
 
     float nextSample()
     {
+        float output{ 0.0f };
+
         phase += inc;
-        if (phase >= 1.0f)
+
+        if (phase <= PI_OVER_4)
         {
-            phase -= 1.0f;
+            float halfPeriod{ period / 2.0f };
+            phaseMax = std::floor(0.5f + halfPeriod) - 0.5f;
+            phaseMax *= PI;
+
+            inc = phaseMax / halfPeriod;
+            phase = -phase;
+
+            if (phase * phase > 1e-9)
+            {
+                output = amplitude * std::sin(phase) / phase;
+            }
+            else
+            {
+                output = amplitude;
+            }
         }
-        return amplitude * std::sin(TWO_PI * phase);
-    }
-
+            else
+        {
+            if (phase > phaseMax)
+            {
+                phase = phaseMax + phaseMax - phase;
+                inc = -inc;
+            }
+            output = amplitude * std::sin(phase) / phase;
+        }
+            return output;
+        }
+    
+   
 private:
-
+    float phase;
+    float phaseMax;
+    float inc;
 };
 
